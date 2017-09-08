f9c3e1b2c7b8352571b7fd05382b8bf67771b491
Henrik Bergqvist <henrik.bergqvist@parttrap.com>
2017-09-08 23:22:19 +0200

    Comments

--- src/ExampleLibrary/TestableClassA.cs
+++ src/ExampleLibrary/TestableClassA.cs
@@ -9,14 +9,14 @@ namespace ExampleLibrary
     public class TestableClassA
     {
 
-        [TestMe.NoParameters(true)]
+        [TestMe.NoParameters(true)] //Sets up a test case that expects this method to return <true>
         public bool ReturnTrue() => true;
 
-        [TestMe.NoParameters(false)]
+        [TestMe.NoParameters(false)] //Sets up a test case that expects this method to return <false>
         public bool ReturnFalse() => false;
 
-        [TestMe.SingleParameter(1, 2)]
-        [TestMe.SingleParameter(2, 4)]
+        [TestMe.SingleParameter(1, 2)] //Sets up a test case that expects this method to return <2> for the parameter <1>
+        [TestMe.SingleParameter(2, 4)] //Sets up a test case that expects this method to return <4> for the parameter <2>
         public int TimesTwo(int value) => value * 2;
 
     }
